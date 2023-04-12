aed13ff0a56d5def195486c468da3b3304add450
itwill <itwill@DESKTOP-70GIBQD>
2023-04-12 18:55:53 +0900

    12Day

--- java_2/src/realization/BoatCarReal.java
+++ java_2/src/realization/BoatCarReal.java
@@ -1,23 +1,18 @@
 package realization;
 
 public class BoatCarReal implements BoatCar {
-
-	@Override//>>실체화
+	@Override
 	public void run() {
-		System.out.println("땅 위를 달리는 능력");
-		
+		System.out.println("땅위를 달리는 능력");
 	}
 
 	@Override
 	public void navigate() {
-		System.out.println("바다를 항해하는 능력");
-	
+		System.out.println("바다를 향해하는 능력");
 	}
 
 	@Override
 	public void floating() {
-		System.out.println("공중에 떠있는 능력");
-		
+		System.out.println("공중에 떠 있는 능력");
 	}
-
 }
