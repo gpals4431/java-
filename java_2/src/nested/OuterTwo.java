e4c7b5ca692042296188cb94b17bae1c9656e56e
itwill <itwill@DESKTOP-70GIBQD>
2023-04-13 19:39:51 +0900

    13Day

--- java_2/src/nested/OuterTwo.java
+++ java_2/src/nested/OuterTwo.java
@@ -6,34 +6,38 @@ public class OuterTwo {
 	public OuterTwo() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public OuterTwo(int outerNum) {
 		super();
 		this.outerNum = outerNum;
 	}
-	
-	public int getOuternum() {
+
+	public int getOuterNum() {
 		return outerNum;
 	}
-	
-	public void setOuternum(int outerNum) {
+
+	public void setOuterNum(int outerNum) {
 		this.outerNum = outerNum;
 	}
 	
 	public void outerDisplay() {
 		System.out.println("outerNum = "+outerNum);
-		/*
-		 * InnerTwo innerTwo = new InnerTwo(200);
-		 * System.out.println("innerNum = "+innerTwo.innerNum); innerTwo.innerDisplay()
-		 */
+		
+		//외부클래스는 정적 내부클래스로 객체를 생성하여 접근제한자에 상관없이 정적 내부클래스의
+		//필드 또는 메소드 참조 가능
+		InnerTwo innerTwo=new InnerTwo(200);
+		System.out.println("innerNum = "+innerTwo.innerNum);
+		innerTwo.innerDisplay();
+		
+		//외부클래스에서는 정적 내부클래스를 사용하여 정적필드 또는 정적메소드 접근 가능 
+		//System.out.println("staticNum = "+InnerTwo.staticNum);
 	}
 	
-	
-	//정적 내부 클래스: static 제한자를 사용하여 클래스 선언
-	//>>정적 내부 클래스에서는 static 제한자를 사용하여 필드 또는 메소드 선언 가능
-	public static class InnerTwo{
+	//정적 내부클래스(Static InnerClass) : static 제한자를 사용하여 클래스 선언
+	// => 정적 내부클래스에서는 static 제한자를 사용하여 필드 또는 메소드 선언 가능
+	public static class InnerTwo {
 		private int innerNum;
-		private static int StaticNum = 300;
+		private static int staticNum = 300;
 		
 		public InnerTwo() {
 			// TODO Auto-generated constructor stub
@@ -53,21 +57,36 @@ public class OuterTwo {
 		}
 
 		public static int getStaticNum() {
-			return StaticNum;
+			return staticNum;
 		}
 
 		public static void setStaticNum(int staticNum) {
-			StaticNum = staticNum;
+			InnerTwo.staticNum = staticNum;
 		}
+		
 		public void innerDisplay() {
 			System.out.println("innerNum = "+innerNum);
-			System.out.println("staticNum = "+StaticNum);
+			System.out.println("staticNum = "+staticNum);
 			
+			//정적 내부클래스에서는 외부클래스의 필드 또는 메소드 참조 불가능
+			// => 외부클래스의 정적필드 또는 정적메소드는 참조 가능
 			//System.out.println("outerNum = "+outerNum);
-			// outerDisplay();
+			//outerDisplay();
+		}
 	}
-
-	}
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java_2/src/xyz/itwill/other/PackageMemberUse.java
@@ -0,0 +1,13 @@
+package xyz.itwill.other;
+
+import xyz.itwill.access.PackageMember;
+
+public class PackageMemberUse {
+	public void run() {
+		PackageMember member = new PackageMember();
+		/*
+		member.num=100;
+		member.display();
+		*/
+	}
+}
