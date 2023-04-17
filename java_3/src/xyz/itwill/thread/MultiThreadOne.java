965ddb74f2eb5cd30d9782b87ffa6a1c077c65b9
itwill <itwill@DESKTOP-70GIBQD>
2023-04-17 19:25:46 +0900

    15Day

--- java_3/src/xyz/itwill/thread/MultiThreadOne.java
+++ java_3/src/xyz/itwill/thread/MultiThreadOne.java
@@ -1,18 +1,16 @@
 /* 0417 4-1*/package xyz.itwill.thread;
 
-public class MultiThreadOne extends Thread{
-	@Override 
-	public void run() {//예외 전달 못함 >> 예외전달하면 오버라이드가아님
-		for(char i='a'; i <'z'; i++) {
+
+public class MultiThreadOne extends Thread {
+	@Override
+	public void run() {
+		for(char i='a';i<='z';i++) {
 			System.out.print(i);
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
-		
 		}
 	}
 }
