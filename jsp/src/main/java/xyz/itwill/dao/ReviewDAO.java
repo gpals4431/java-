021f75ede9f75debae34ae8b8628ff1acb45882e
조혜민 <@211.219.179.210>
2023-06-30 18:30:04 +0900

    2023-06-30

--- jsp/src/main/java/xyz/itwill/dao/ReviewDAO.java
+++ jsp/src/main/java/xyz/itwill/dao/ReviewDAO.java
@@ -74,24 +74,22 @@ public class ReviewDAO extends JdbcDAO {
 				
 				//REVIEW 테이블에 저장된 전체 게시글의 갯수를 검색
 				//select * from 안쓴이유 => id라는 컬럼 중복 , 어느 테이블의 컬럼값인지 표시해야함
-				String sql="select * from (select rownum rn, temp.* from (select num, member.id"
-						+ ", name, subject, content, regdate, readcount, ref, restep, relevel"
-						+ ", ip, status from review join member on review.id=member.id "
-						+ "order by ref desc, restep) temp) where rn between ? and ?";
+				String sql="select * from (select rownum rn, temp.* from (select num, reviewid"
+						+ ", name, subject, content, reviewimg, regdate, readcount, ref, restep"
+						+ ", relevel,ip, status from review join member on reviewid=id order by"
+						+ " ref desc, restep) temp) where rn between ? and ?";
 					pstmt=con.prepareStatement(sql);
 					pstmt.setInt(1, startRow);
 					pstmt.setInt(2, endRow);
 				
 			}else {//게시글 검색 기능을 사용한 경우
 				//검색대상(컬럼명)에 검색단어가 포함한 게시글 갯수 검색  - 삭제글 제외
-				String sql="select * from (select rownum rn, temp.* from (select num, member.id"
-						+ ", name, subject, content, regdate, readcount, ref, restep, relevel"
-						+ ", ip, status from review join member on review.id=member.id"
-						+ " where "+search+" like '%'||?||'%' and status <> 0"
-						+ " order by ref desc, restep) temp) where rn between ? and ?";
+				String sql="select * from (select rownum rn, temp.* from (select num, reviewid"
+						+ ", name, subject, content, reviewimg, regdate, readcount, ref, restep"
+						+ ", relevel,ip, status from review join member on reviewid=id where "
+						+ search + " like '%'||?||'%' and status <> 0 order by ref desc, restep)"
+						+ " temp) where rn between ? and ?";
 					pstmt=con.prepareStatement(sql);
-					
-					
 					pstmt.setString(1, keyword);
 					pstmt.setInt(2, startRow);
 					pstmt.setInt(3, endRow);
@@ -99,13 +97,15 @@ public class ReviewDAO extends JdbcDAO {
 			
 			rs=pstmt.executeQuery();
 			
+			
 			while(rs.next()) {
 				ReviewDTO review=new ReviewDTO();
 				review.setNum(rs.getInt("num"));
-				review.setId(rs.getString("id"));
+				review.setReviewid(rs.getString("reviewid"));
 				review.setName(rs.getString("name"));
 				review.setSubject(rs.getString("subject"));
 				review.setContent(rs.getString("content"));
+				review.setReviewimg(rs.getString("reviewimg"));
 				review.setRegdate(rs.getString("regdate"));
 				review.setReadcount(rs.getInt("readcount"));
 				review.setRef(rs.getInt("ref"));
@@ -140,13 +140,14 @@ public class ReviewDAO extends JdbcDAO {
 			if(rs.next()) {
 				nextNum=rs.getInt(1);
 			}
-		}catch (SQLException e) {
-				System.out.println("[에러]selectNextNum()메소드에 의한 SQL오류"+e.getMessage());
-		}finally {
-				close(con, pstmt, rs);
-		}return nextNum;
-	
+		} catch (SQLException e) {
+			System.out.println("[에러]selectNextNum() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return nextNum;
 	}
+	
 	
 	//게시글 정보를 전달받아 review 테이블에 삽입하고 삽입행의 갯수를 반환하는 메소드
 	public int insertReview(ReviewDTO review) {
@@ -156,17 +157,18 @@ public class ReviewDAO extends JdbcDAO {
 		try {
 			con=getConnection();
 			
-			String sql="insert into review values(?,?,?,?,sysdate,0,?,?,?,?,?)";
+			String sql="insert into review values(?,?,?,?,?,sysdate,0,?,?,?,?,?)";
 			pstmt=con.prepareStatement(sql);
 			pstmt.setInt(1, review.getNum());
-			pstmt.setString(2, review.getId());
+			pstmt.setString(2, review.getReviewid());
 			pstmt.setString(3, review.getSubject());
 			pstmt.setString(4, review.getContent());
-			pstmt.setInt(5, review.getRef());
-			pstmt.setInt(6, review.getRestep());
-			pstmt.setInt(7, review.getRelevel());
-			pstmt.setString(8, review.getIp());
-			pstmt.setInt(9, review.getStatus());
+			pstmt.setString(5, review.getReviewimg());
+			pstmt.setInt(6, review.getRef());
+			pstmt.setInt(7, review.getRestep());
+			pstmt.setInt(8, review.getRelevel());
+			pstmt.setString(9, review.getIp());
+			pstmt.setInt(10, review.getStatus());
 			
 			rows=pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -177,4 +179,30 @@ public class ReviewDAO extends JdbcDAO {
 		return rows;
 	
 	}
+	
+	/*
+	부모글 관련 정보를 전달받아 REVIEW 테이블에 저장된 게시글에서 부모글의 글그룹과 같은
+	게시글 중 부모글의 글 순서보다 큰 게시글의 RESTEP 컬럼값을 +1증가되도록 변경하고 변경행의 갯수를 반환하는 메소드
+	 */
+	public int updateRestep(int ref, int restep) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int rows=0;
+		try {
+			con=getConnection();
+			//최신글이 맨 위로 올라오려면 where ref=? and restep(=글순서)>? 넣어줘야함
+			String sql="update review set restep=restep+1 where ref=? and restep>?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1,ref);
+			pstmt.setInt(2,restep);
+			
+			rows=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]insertReview() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;	
+	}
+		
 }
