69f13de726cb14d221ac4e200c35e0adc587cedd
sample <user@BOOK-LCCUUB9T5H>
2023-07-27 11:21:30 +0900

    2023-07-27

---
+++ spring/src/main/java/xyz/itwill04/bean/AnnotationBean.java
@@ -0,0 +1,11 @@
+package xyz.itwill04.bean;
+
+public class AnnotationBean {
+	public AnnotationBean() {
+		System.out.println("### AnnotationBean 클래스의 기본 생성자 호출 ###");
+	}
+	public void display() {
+		System.out.println("### AnnotationBean 클래스의 display메소드 호출 ###");
+
+	}
+}
